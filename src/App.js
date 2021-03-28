964d56a10bb9e0aee3c7c95275d312343b0d34b3
Ketan Chavda <chavda.ketan@gmail.com>
2021-03-27 21:23:06 -0400

    Installed react-router-dom,@material-ui/core @material-ui/icons, Home component and added Route in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Home from "./components/Home";
 import "./App.css";
 
 function App() {
-  return <div className="app">React App</div>;
+  return (
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
